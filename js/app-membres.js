ca87662378d5c4f8230ecb7b0d8375004d482071
Eolift-Racing <70943770+Eolift-Racing@users.noreply.github.com>
2020-09-10 13:50:03 +0200

    Update app-membres.js

--- js/app-membres.js
+++ js/app-membres.js
@@ -22,13 +22,22 @@ $(document).ready(function() {
 		
 
 	}, function() {
-		$('#photo2').attr('hidden', true);
+		$('#photo2').attr('hidden', false);
 	});
 
-		$('.membre2').on('click', function(e) {
-		$(this).next('ul').toggleClass('open');
-		e.preventDefault(); 
-		return false;
+
+
+	$('.membre2').hover(
+
+		function() {
+		var memberId;
+		memberId = $(this).attr('id');
+		$('#photo').attr('hidden',false);
+		$('#photo').attr('src','images/voile/membres/'+memberId+'.jpg');
+		
+
+	}, function() {
+		$('#photo').attr('hidden', true);
 	});
 
-});
+	});
